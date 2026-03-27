13ee835312b7f29dfdf5fa35d8f0cce9fb8cd57f
Morbanaa <theodorerodd@gmail.com>
2026-03-27 18:53:31 -0500

    Added parent weapons class and created contructor as well as abstract method

--- classes.py
+++ classes.py
@@ -1,1 +1,9 @@
-ECHO is on.
+from abc import ABC, abstractmethod
+
+class Weapons(ABC):
+    def __main__(self,name):
+        self.name = name
+
+    @abstractmethod
+    def attack():
+        pass
