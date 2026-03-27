64082f499f2915d6c797f4b54993098fe48ca245
Morbanaa <theodorerodd@gmail.com>
2026-03-27 18:57:35 -0500

    Added sub classes sword and bow

--- classes.py
+++ classes.py
@@ -3,7 +3,17 @@ from abc import ABC, abstractmethod
 class Weapons(ABC):
     def __main__(self,name):
         self.name = name
-
+    
+    # All sub classes need this method attack
     @abstractmethod
     def attack():
         pass
+
+class Sword(Weapons):
+    def attack():
+        print("The player swings their sword!")
+
+
+class Bow(Weapons):
+    def attack():
+        print("The player fires their bow!")
