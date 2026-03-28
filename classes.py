598536c20c28d06c5bca83c10b09a4f6b81926c9
Morbanaa <theodorerodd@gmail.com>
2026-03-27 19:13:26 -0500

    Fixed bug in Weapons class, changed __main__ to __init__

--- classes.py
+++ classes.py
@@ -1,25 +1,25 @@
 from abc import ABC, abstractmethod
 
 class Weapons(ABC):
-    def __main__(self,name):
+    def __init__(self,name):
         self.name = name
     
     # All sub classes need this method attack
     @abstractmethod
-    def attack():
+    def attack(self):
         pass
 
 class Sword(Weapons):
-    def attack():
-        print("The player swings their sword!")
+    def attack(self):
+        print(f"The player swings their sword named {self.name}!")
 
 
 class Bow(Weapons):
-    def attack():
-        print("The player fires their bow!")
+    def attack(self):
+        print(f"The player fires their bow named {self.name}!")
 
 
 class Staff(Weapons):
-    def attack():
-        print("The player casts a fireball with their staff!")
+    def attack(self):
+        print(f"The player casts a fireball with their staff named {self.name}!")
 
