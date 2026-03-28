f3cb0b5753e8637f0d178fc23145d512259265e8
Morbanaa <theodorerodd@gmail.com>
2026-03-27 19:17:06 -0500

    Created a switch cases to call class methods based on user input

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from classes import Weapons, Sword, Bow, Staff
 
 def main():
+    # Creates objects for later use
     sword = Sword("Deathbane")
     bow = Bow("Cupid")
     staff = Staff("Groot")
 
+    # Prints user options
     while True:
         print("Choose:")
         print("(1) Swing Sword:")
         print("(2) Shoot Bow:")
         print("(3) Shoot Firebal:l")
         choice = input("Choose: ")
+
+        match(choice):
+            case "1":
+                sword.attack()
+            case "2":
+                bow.attack()
+            case "3":
+                staff.attack()
+            case _:
+                continue
 
 # Program Entry Point
 if __name__ == "__main__":
