5e58c3fbe3dcb21b9d9cdafb74c79a6cec9ca868
Morbanaa <theodorerodd@gmail.com>
2026-03-27 19:12:22 -0500

    Added UI for later user choice, also created objects of the classes

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from classes import Weapons, Sword, Bow
+from classes import Weapons, Sword, Bow, Staff
 
 def main():
-    pass
+    sword = Sword("Deathbane")
+    bow = Bow("Cupid")
+    staff = Staff("Groot")
+
+    while True:
+        print("Choose:")
+        print("(1) Swing Sword:")
+        print("(2) Shoot Bow:")
+        print("(3) Shoot Firebal:l")
+        choice = input("Choose: ")
 
 # Program Entry Point
 if __name__ == "__main__":
